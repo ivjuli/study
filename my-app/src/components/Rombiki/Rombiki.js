7c2d49918222c0e735de879953775a940a87cdfe
ivjuli <ivjuli2@gmail.com>
2019-03-06 00:31:15 +0300

    Create Rombiki.js

---
+++ my-app/src/components/Rombiki/Rombiki.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Rombiki = () => {
+return (
+<div className="rombiki_raspisanie">
+    <div className="rombiki_raspisanie1"></div>
+    <div className="rombiki_raspisanie2">
+    <div className="rombiki_raspisanie2_1"></div></div>
+    <div className="rombiki_raspisanie3"> 
+    <div className="rombiki_raspisanie3_1"></div></div>
+    <div className="rombiki_raspisanie4"></div>
+    <div className="rombiki_raspisanie5">
+    <div className="rombiki_raspisanie5_1"></div></div>
+    <div className="rombiki_raspisanie6">
+    <div className="rombiki_raspisanie6_1"></div></div>
+    <div className="rombiki_raspisanie7"></div>
+    <div className="rombiki_raspisanie8"></div>
+ </div>
+
+)
+
+}
+
+export default Rombiki
