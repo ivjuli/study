329e33eb4a7a8dbdb775dccd37dd1ead5ad9aff9
ivjuli <ivjuli2@gmail.com>
2019-03-11 14:49:30 +0300

    Create Footer.js

---
+++ my-app/src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+const Footer = () => {
+return (
+<div className="footer">
+<div className="romb_footer"></div>
+<div className="romb_footer_logo">
+<img src="/img/mark3_white.svg" alt="logo" width="61px" height="61px"/>
+<div className="tex_foter"> NORN<br/>
+школа скандинавских языков</div>
+</div>
+<div className="tex_foter footer_tex"> 
+<p>199983, Санкт-Петербург<br/>
+ул. Казанская, 32, БЦ “Успех”, 7 этаж<br/>
+c 10:00 до 22:00 с ПН по СБ</p>
+    
+<p>orn@gmail.com<br/>
++ 7 893 567 78 67</p>
+</div>
+<div className="flex_text_footer">
+<div className="tex_foter line_height">
+<div className="margin">Шведский язык</div>
+<div className="margin">Норвежский язык</div>
+<div className="margin">Исландский язык</div>
+<div className="margin">Финский язык</div>
+<div className="margin">Датский язык</div>
+<div className="margin">Тестирование</div>
+<div className="margin">Формы обучения</div>
+</div>
+<div className="tex_foter line_height">
+<div className="margin">Аудитории</div>
+<div className="margin">Преподаватели</div>
+<div className="margin">Летняя школа</div>
+<div className="margin">Отзывы</div>
+<div className="margin">Политика<br/>конфиденциальности</div>
+<div className="margin">FAQ</div>
+</div>
+</div> </div>
+)
+}
+
+export default Footer
