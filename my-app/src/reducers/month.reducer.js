4a249396bd6bb60571d0daac05db72e1bffa8171
ivjuli <ivjuli2@gmail.com>
2019-03-13 17:09:32 +0300

    Create month.reducer.js

---
+++ my-app/src/reducers/month.reducer.js
@@ -0,0 +1,60 @@
+const iniState = [
+    {
+     "idMonth": 1,
+     "nameMonth": "ЯНВАРЬ"
+   },
+   
+   {
+     "idMonth": 2,
+     "nameMonth": "ЯНВАРЬ"
+   },
+   {
+     "idMonth": 3,
+     "nameMonth": "МАРТ"
+   },
+   {
+     "idMonth": 4,
+     "nameMonth": "АПРЕЛЬ"
+   },
+   {
+     "idMonth": 5,
+     "nameMonth": "МАЙ"
+   },
+   {
+     "idMonth": 6,
+     "nameMonth": "ИЮНЬ"
+   },
+   {
+     "idMonth": 7,
+     "nameMonth": "ИЮЛЬ"
+   },
+   {
+     "idMonth": 8,
+     "nameMonth": "АВГУСТ"
+   },
+   {
+     "idMonth": 9,
+     "nameMonth": "СЕНТРЯБРЬ"
+   },
+   {
+     "idMonth": 10,
+     "nameMonth": "ОКТЯБРЬ"
+   },
+   {
+     "idMonth": 11,
+     "nameMonth": "НОЯБРЬ"
+   },
+   {
+     "idMonth": 12,
+     "nameMonth": "ДЕКАБРЬ"
+   }
+   ]
+   
+
+   export default function month(state=iniState, action) {
+       switch (action.type) {
+           default: 
+           return state;
+       }
+   }
+   
