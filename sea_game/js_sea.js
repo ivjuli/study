405d68f5c27e60390e05bd8ae5ded5333df9ef7c
ivjuli <ivjuli2@gmail.com>
2019-03-15 20:21:29 +0300

    Create js_sea.js

---
+++ sea_game/js_sea.js
@@ -0,0 +1,31 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt('Введите координату! (цифру от 0 до 6)');
+    if (guess < 0 || guess > 6) {
+        alert('Координаты некорректны! Введите цифру от 0 до 6');
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert('Поподание!');
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert('Корабль потоплен!');
+            }
+        } else {
+            alert('Мимо!');
+        }
+    }
+}
+
+var stats = 'Вы использовали ' + guesses + ' выстрелов, чтобы потопить корабль, ' + 'ваша точность поподаний ' + (3/guesses);
+
+alert(stats);
