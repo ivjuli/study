5cbed16cf6704b78dfb20e2d9cf6cb1378594395
ivjuli <ivjuli2@gmail.com>
2019-03-22 19:05:48 +0300

    Update api.js

--- my-app/src/helpers/api.js
+++ my-app/src/helpers/api.js
@@ -222,10 +222,70 @@ const iniState = [
      "startData": "31.07.",
      "namelanguage": "исландский",
      "level": "В1"
-    }
+    }/*
    ]
+,
+ [
+    {
+     "idMonth": 1,
+     "nameMonth": "ЯНВАРЬ"
+   },
+   
+   {
+     "idMonth": 2,
+     "nameMonth": "ФЕВРАЛЬ"
+   },
+   {
+     "idMonth": 3,
+     "nameMonth": "МАРТ"
+   },
+   {
+     "idMonth": 4,
+     "nameMonth": "АПРЕЛЬ"
+   },
+   {
+     "idMonth": 5,
+     "nameMonth": "МАЙ"
+   },
+   {
+     "idMonth": 6,
+     "nameMonth": "ИЮНЬ"
+   },
+   {
+     "idMonth": 7,
+     "nameMonth": "ИЮЛЬ"
+   },
+   {
+     "idMonth": 8,
+     "nameMonth": "АВГУСТ"
+   },
+   {
+     "idMonth": 9,
+     "nameMonth": "СЕНТРЯБРЬ"
+   },
+   {
+     "idMonth": 10,
+     "nameMonth": "ОКТЯБРЬ"
+   },
+   {
+     "idMonth": 11,
+     "nameMonth": "НОЯБРЬ"
+   },
+   {
+     "idMonth": 12,
+     "nameMonth": "ДЕКАБРЬ"
+   }
+]*/]
+   
 
 export const getAllLanguage = async () => {
     const language = await fetch(iniState);
 
+
 };
+
+/*export const getAllmonth = async () => {
+    const month = await fetch(iniStatem);
+
+};*/
+
